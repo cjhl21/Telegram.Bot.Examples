e25a9f2e0762fbf0e790489c6505c63922eaec87
cjhl21 <caohai4078@126.com>
2025-03-08 14:46:43 +0800

    feat: 导入tgbot demo

---
+++ Haicao.WebApplication/BotConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Haicao.WebApplication;
+
+public class BotConfiguration
+{
+    public string BotToken { get; init; } = default!;
+    public Uri BotWebhookUrl { get; init; } = default!;
+    public string SecretToken { get; init; } = default!;
+}
